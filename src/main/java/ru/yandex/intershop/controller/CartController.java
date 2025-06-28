df106ca23d61d42deaff374f1de70731d8c72caf
Khazanov Sergei <asd@asd>
2025-06-28 21:42:40 +0300

    эндпоинт отображения корзины и обновления кол-ва товара

--- src/main/java/ru/yandex/intershop/controller/CartController.java
+++ src/main/java/ru/yandex/intershop/controller/CartController.java
@@ -1,43 +1,60 @@
-//package ru.yandex.intershop.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import ru.yandex.intershop.enums.ActionType;
-//import ru.yandex.intershop.model.Cart;
-//import ru.yandex.intershop.model.Item;
-//import ru.yandex.intershop.service.CartService;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Controller
-//@RequestMapping("/cart/items")
-//@RequiredArgsConstructor
-//public class CartController {
-//    private final CartService cartService;
-//
-//    @GetMapping
-//    public String showCart(Model model) {
-//        Cart cart = cartService.getCurrentUserCart();
-//        List<Item> items = cart.getItems().stream().map(cartItem -> {
-//            Item item = cartItem.getItem();
-//            item.setCount(cartItem.getQuantity());
-//            return item;
-//        }).collect(Collectors.toList());
-//        model.addAttribute("items", items);
-//        model.addAttribute("total", cartService.calculateTotal(cart));
-//        model.addAttribute("empty", cart.getItems().isEmpty());
-//        return "cart";
-//    }
-//
-//    @PostMapping("/{id}")
-//    public String updateCartItem(
-//            @PathVariable Long id,
-//            @RequestParam ActionType action
-//    ) {
-//        cartService.updateCartItem(id, action);
-//        return "redirect:/cart/items";
-//    }
-//}
+package ru.yandex.intershop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.yandex.intershop.dto.UpdateCartForm;
+import ru.yandex.intershop.model.Cart;
+import ru.yandex.intershop.model.Item;
+import ru.yandex.intershop.service.CartService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Controller
+@RequestMapping("/cart/items")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @GetMapping
+    public Mono<String> showCart(Model model) {
+        return cartService.getCurrentUserCart()
+                .flatMap(cart -> prepareCartView(cart, model)
+                        .thenReturn("cart"));
+    }
+
+    private Mono<Void> prepareCartView(Cart cart, Model model) {
+        return transformCartItems(cart)
+                .collectList()
+                .flatMap(items -> cartService.calculateTotal(cart)
+                        .doOnNext(total -> addAttributesToModel(cart, items, total, model))
+                        .then());
+    }
+
+    private Flux<Item> transformCartItems(Cart cart) {
+        return Flux.fromIterable(cart.getItems())
+                .map(cartItem -> {
+                    Item item = cartItem.getItem();
+                    item.setCount(cartItem.getQuantity());
+                    return item;
+                });
+    }
+
+    private void addAttributesToModel(Cart cart, List<Item> items, BigDecimal total, Model model) {
+        model.addAttribute("items", items);
+        model.addAttribute("total", total);
+        model.addAttribute("empty", cart.getItems().isEmpty());
+    }
+
+    @PostMapping("/{id}")
+    public Mono<String> updateCartItem(
+            @PathVariable Long id,
+            @ModelAttribute UpdateCartForm form) {
+        return cartService.updateCartItem(id, form.getAction())
+                .then(Mono.just("redirect:/cart/items"));
+    }
+}
